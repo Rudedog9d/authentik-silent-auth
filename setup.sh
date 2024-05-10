0657e7bc54e4365072ed5d622c85718f28a5fc72
Brodie Davis <bdavis@c2games.org>
2024-05-10 14:19:40 -0400

    add traefik config for configuration of CSP header

--- setup.sh
+++ setup.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-curl -sL https://goauthentik.io/docker-compose.yml -o docker-compose.yml
+curl -sL https://goauthentik.io/docker-compose.yml -o docker-compose.authentik.yml
 
 declare -A env
 env["PG_PASS"]=`openssl rand -base64 36`
 env["AUTHENTIK_SECRET_KEY"]=`openssl rand -base64 36`
 env["AUTHENTIK_ERROR_REPORTING_ENABLED"]="true"
-env["COMPOSE_PORT_HTTP"]="80"
-env["COMPOSE_PORT_HTTPS"]="443"
 env["AUTHENTIK_BOOTSTRAP_PASSWORD"]="admin"
 env["AUTHENTIK_BOOTSTRAP_TOKEN"]=`openssl rand -base64 36`
 env["AUTHENTIK_BOOTSTRAP_EMAIL"]="admin@example.com"
 env["AUTHENTIK_TOKEN"]=${env["AUTHENTIK_BOOTSTRAP_TOKEN"]}
 env["AUTHENTIK_INSECURE"]="true"
 env["AUTHENTIK_URL"]="https://auth.local.malteksolutions.com"
+env["BASE_DOMAIN"]="local.malteksolutions.com"
 
 for K in ${!env[@]}
 do
